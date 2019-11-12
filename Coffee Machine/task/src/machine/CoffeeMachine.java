012e9b888793409b7d2cf63950bd9355eafd557e
Von_C <Von.Combinersky@gmail.com>
2019-11-12 22:57:48 +0100

    Stage #1 HelloCoffe main app class set up and some basic printouts

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,13 @@
 package machine;
 
 public class CoffeeMachine {
-    public static void main(String[] args)
-    System.out.println("Hello World!");
+    public static void main(String[] args){
+    System.out.print("Starting to make a coffee\n"+
+                        "Grinding coffee beans\n"+
+                        "Boiling water\n"+
+                        "Mixing boiled water with crushed coffee beans\n"+
+                        "Pouring coffee into the cup\n"+
+                        "Pouring some milk into the cup\n"+
+                        "Coffee is ready!");
     }
 }
